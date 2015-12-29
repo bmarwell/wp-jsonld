17f56e99afd460a635a2f1cdc4b54ce6ffdb613c
Benjamin Marwell <bmarwell@gmail.com>
2015-12-29 15:22:18 +0100

    add classes for json like in bmhm/webtrees-jsonld.

---
+++ Author.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Copyright (C) 2015 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Class: Author.
+ *
+ * Represents a Person who wrote an Article.
+ *
+ * @see JsonLD
+ */
+class Author extends JsonLD
+{
+
+    /**
+     * email
+     *
+     * @var String
+     */
+    public $email;
+
+    /**
+     * name
+     *
+     * @var String
+     */
+    public $name;
+
+    /**
+     * url
+     *
+     * The author page in wordpress.
+     *
+     * @var String
+     */
+    public $url;
+
+    /**
+      * @param bool|FALSE $addContext
+      */
+    public function __construct($addContext = false)
+    {
+        parent::__construct("Person", $addContext);
+    }
+}
+
+
--- json-ld-article.php
+++ json-ld-article.php
@@ -31,21 +31,48 @@ with JSON-LD for Aricle; if not, write to the Free Software Foundation, Inc.,
 
 
 /**
+ * createArticle
+ *
+ * @param bool|FALSE $isParent
+ */
+function createArticle($isParent = false) {
+
+    $article = new Article(isParent);
+    $article->headline = get_the_title();
+    $article->datePublished = get_the_date('Y-n-j');
+    $article->url = get_permalink();
+
+    return $article;
+}
+
+/**
+ * createAuthorMarkup - create Author Markup
+ *
+ * @param bool|FALSE $isParent
+ */
+function createAuthor($isParent = false) {
+    $author = new Author(isParent);
+    $author->name = get_the_author();
+    $author->url = get_the_author_meta("user_url");
+
+    return $author;
+}
+
+/**
  * Echoes Markup to your footer.
  * @author Mikko Piippo, Tomi Lattu
  * @since 0.1
  */
 function add_markup() {
+    $markup = null;
 
-    //
     // Get the data needed for building the JSON-LD
-    //
     if (is_single()) {
-        $articletitle=get_the_title();
-        $articleauthor=get_the_author();
-        $articlepublished=get_the_date('Y-n-j');
+        $markup = createArticleMarkup(true);
+        $markup->author = createAuthor();
+
+        // TODO: Implement everything below.
         $articlepublisher=get_bloginfo('name');
-        $articleurl=get_permalink();
         $articlesection=get_the_category()[0]->cat_name;
         $articlemodified=get_the_modified_date();
         $articlecommentcount=get_comments_number();
@@ -54,30 +81,14 @@ function add_markup() {
             $thumbnailurl=wp_get_attachment_url(get_post_thumbnail_id());
         }
 
-        $author = array(
-            '@type' => 'Person',
-            'name'  => $articleauthor);
-
         $pub = array (
             '@type' => 'Organization',
             'name'  => $articlepublisher);
-
-        $arr = array(
-            '@context' => 'http://schema.org',
-            '@type'    => 'Article',
-            'headline'     => $articletitle,
-            'author'   => $author,
-            'datePublished' => $articlepublished,
-            'articleSection'  => $articlesection,
-            'url'      => $articleurl,
-            'image'  => $thumbnailurl,
-            'publisher' =>$pub);
-
-        echo '<script type="application/ld+json">'
-            . json_encode($arr,JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT)
-            . '</script>';
     } //end if single
 
+    echo '<script type="application/ld+json">'
+        . json_encode($markup, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT)
+        . '</script>';
 } // end function
 add_action ('wp_footer','add_markup');
 
